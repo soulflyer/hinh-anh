89be73babd1b7c644d33d19e1c619b12bb56f67f
iain <iain@soulflyer.co.uk>
2018-02-27 13:29:45 +0700

    reformat project.clj

--- project.clj
+++ project.clj
@@ -20,141 +20,149 @@
   :profiles {:dev {:dependencies [[figwheel "0.5.11"]
                                   [com.cemerick/piggieback "0.2.2"]
                                   [figwheel-sidecar "0.5.11"]]}}
-  :aliases {"descjop-help" ["new" "descjop" "help"]
-            "descjop-version" ["new" "descjop" "version"]
-            "descjop-init" ["do"
-                            ["shell" "npm" "install"]
-                            ["shell" "grunt" "download-electron"]]
-            "descjop-init-win" ["do"
-                                ["shell" "cmd.exe" "/c" "npm" "install"]
-                                ["shell" "cmd.exe" "/c" "grunt" "download-electron"]]
-            "descjop-externs" ["do"
-                               ["externs" "dev-main" "app/dev/js/externs.js"]
-                               ["externs" "dev-front" "app/dev/js/externs_front.js"]
-                               ["externs" "prod-main" "app/prod/js/externs.js"]
-                               ["externs" "prod-front" "app/prod/js/externs_front.js"]]
-            "descjop-externs-dev" ["do"
-                                   ["externs" "dev-main" "app/dev/js/externs.js"]
-                                   ["externs" "dev-front" "app/dev/js/externs_front.js"]]
-            "descjop-externs-prod" ["do"
-                                    ["externs" "prod-main" "app/prod/js/externs.js"]
-                                    ["externs" "prod-front" "app/prod/js/externs_front.js"]]
-            "descjop-figwheel" ["trampoline" "figwheel" "dev-front"]
-            "descjop-once" ["do"
-                            ["cljsbuild" "once" "dev-main"]
-                            ["cljsbuild" "once" "dev-front"]
-                            ["cljsbuild" "once" "prod-main"]
-                            ["cljsbuild" "once" "prod-front"]]
-            "descjop-once-dev" ["do"
-                                ["cljsbuild" "once" "dev-main"]
-                                ["cljsbuild" "once" "dev-front"]]
-            "descjop-once-prod" ["do"
-                                 ["cljsbuild" "once" "prod-main"]
-                                 ["cljsbuild" "once" "prod-front"]]
-            ;; electron packager for production
-            "descjop-uberapp-osx" ["shell" "electron-packager" "./app/prod" "anh" "--platform=darwin" "--arch=x64" "--electron-version=1.6.6"]
-            "descjop-uberapp-app-store" ["shell" "electron-packager" "./app/prod" "anh" "--platform=mas" "--arch=x64" "--electron-version=1.6.6"]
-            "descjop-uberapp-linux" ["shell" "electron-packager" "./app/prod" "anh" "--platform=linux" "--arch=x64" "--electron-version=1.6.6"]
-            "descjop-uberapp-win64" ["shell" "cmd.exe" "/c" "electron-packager" "./app/prod" "anh" "--platform=win32" "--arch=x64" "--electron-version=1.6.6"]
-            "descjop-uberapp-win32" ["shell" "cmd.exe" "/c" "electron-packager" "./app/prod" "anh" "--platform=win32" "--arch=ia32" "--electron-version=1.6.6"]
-            }
+  :aliases
+  {"descjop-help" ["new" "descjop" "help"]
+   "descjop-version" ["new" "descjop" "version"]
+   "descjop-init" ["do"
+                   ["shell" "npm" "install"]
+                   ["shell" "grunt" "download-electron"]]
+   "descjop-init-win" ["do"
+                       ["shell" "cmd.exe" "/c" "npm" "install"]
+                       ["shell" "cmd.exe" "/c" "grunt" "download-electron"]]
+   "descjop-externs" ["do"
+                      ["externs" "dev-main" "app/dev/js/externs.js"]
+                      ["externs" "dev-front" "app/dev/js/externs_front.js"]
+                      ["externs" "prod-main" "app/prod/js/externs.js"]
+                      ["externs" "prod-front" "app/prod/js/externs_front.js"]]
+   "descjop-externs-dev" ["do"
+                          ["externs" "dev-main" "app/dev/js/externs.js"]
+                          ["externs" "dev-front" "app/dev/js/externs_front.js"]]
+   "descjop-externs-prod" ["do"
+                           ["externs" "prod-main" "app/prod/js/externs.js"]
+                           ["externs" "prod-front" "app/prod/js/externs_front.js"]]
+   "descjop-figwheel" ["trampoline" "figwheel" "dev-front"]
+   "descjop-once" ["do"
+                   ["cljsbuild" "once" "dev-main"]
+                   ["cljsbuild" "once" "dev-front"]
+                   ["cljsbuild" "once" "prod-main"]
+                   ["cljsbuild" "once" "prod-front"]]
+   "descjop-once-dev" ["do"
+                       ["cljsbuild" "once" "dev-main"]
+                       ["cljsbuild" "once" "dev-front"]]
+   "descjop-once-prod" ["do"
+                        ["cljsbuild" "once" "prod-main"]
+                        ["cljsbuild" "once" "prod-front"]]
+   ;; electron packager for production
+   "descjop-uberapp-osx" ["shell" "electron-packager" "./app/prod" "anh"
+                          "--platform=darwin" "--arch=x64" "--electron-version=1.6.6"]
+   "descjop-uberapp-app-store" ["shell" "electron-packager" "./app/prod" "anh"
+                                "--platform=mas" "--arch=x64" "--electron-version=1.6.6"]
+   "descjop-uberapp-linux" ["shell" "electron-packager" "./app/prod" "anh"
+                            "--platform=linux" "--arch=x64" "--electron-version=1.6.6"]
+   "descjop-uberapp-win64" ["shell" "cmd.exe" "/c" "electron-packager" "./app/prod" "anh"
+                            "--platform=win32" "--arch=x64" "--electron-version=1.6.6"]
+   "descjop-uberapp-win32" ["shell" "cmd.exe" "/c" "electron-packager" "./app/prod" "anh"
+                            "--platform=win32" "--arch=ia32" "--electron-version=1.6.6"]
+   }
   :hooks [leiningen.cljsbuild]
-  :cljsbuild {:builds {:dev-main {:source-paths ["src"]
-                                  :incremental true
-                                  :jar true
-                                  :assert true
-                                  :compiler {:output-to "app/dev/js/cljsbuild-main.js"
-                                             :externs ["app/dev/js/externs.js"
-                                                       "node_modules/closurecompiler-externs/path.js"
-                                                       "node_modules/closurecompiler-externs/process.js"]
-                                             :warnings true
-                                             :elide-asserts true
-                                             :target :nodejs
+  :cljsbuild
+  {:builds
+   {:dev-main {:source-paths ["src"]
+               :incremental true
+               :jar true
+               :assert true
+               :compiler {:output-to "app/dev/js/cljsbuild-main.js"
+                          :externs ["app/dev/js/externs.js"
+                                    "node_modules/closurecompiler-externs/path.js"
+                                    "node_modules/closurecompiler-externs/process.js"]
+                          :warnings true
+                          :elide-asserts true
+                          :target :nodejs
 
-                                             ;; no optimize compile (dev)
-                                             ;; :optimizations :none
-                                             :output-dir "app/dev/js/out_main"
+                          ;; no optimize compile (dev)
+                          ;; :optimizations :none
+                          :output-dir "app/dev/js/out_main"
 
-                                             ;; simple compile (dev)
-                                             :optimizations :simple
+                          ;; simple compile (dev)
+                          :optimizations :simple
 
-                                             ;; advanced compile (prod)
-                                             ;;:optimizations :advanced
+                          ;; advanced compile (prod)
+                          ;;:optimizations :advanced
 
-                                             ;;:source-map "app/dev/js/test.js.map"
-                                             :pretty-print true
-                                             :output-wrapper true}}
-                       :dev-front {:source-paths ["src_front" "src_front_profile/anh_front/dev"]
-                                   :incremental true
-                                   :jar true
-                                   :assert true
-                                   :compiler {:output-to "app/dev/js/front.js"
-                                              :externs ["app/dev/js/externs_front.js"]
-                                              :warnings true
-                                              :elide-asserts true
-                                              ;; :target :nodejs
+                          ;;:source-map "app/dev/js/test.js.map"
+                          :pretty-print true
+                          :output-wrapper true}}
+    :dev-front {:source-paths ["src_front" "src_front_profile/anh_front/dev"]
+                :incremental true
+                :jar true
+                :assert true
+                :compiler {:output-to "app/dev/js/front.js"
+                           :externs ["app/dev/js/externs_front.js"]
+                           :warnings true
+                           :elide-asserts true
+                           ;; :target :nodejs
 
-                                              ;; no optimize compile (dev)
-                                              :optimizations :none
-                                              :output-dir "app/dev/js/out_front"
+                           ;; no optimize compile (dev)
+                           :optimizations :none
+                           :output-dir "app/dev/js/out_front"
 
-                                              ;; simple compile (dev)
-                                              ;;:optimizations :simple
+                           ;; simple compile (dev)
+                           ;;:optimizations :simple
 
-                                              ;; advanced compile (prod)
-                                              ;;:optimizations :advanced
+                           ;; advanced compile (prod)
+                           ;;:optimizations :advanced
 
-                                              ;;:source-map "app/dev/js/test.js.map"
-                                              :pretty-print true
-                                              :output-wrapper true}}
-                       :prod-main {:source-paths ["src"]
-                                   :incremental true
-                                   :jar true
-                                   :assert true
-                                   :compiler {:output-to "app/prod/js/cljsbuild-main.js"
-                                              :externs ["app/prod/js/externs.js"
-                                                        "node_modules/closurecompiler-externs/path.js"
-                                                        "node_modules/closurecompiler-externs/process.js"]
-                                              :warnings true
-                                              :elide-asserts true
-                                              :target :nodejs
+                           ;;:source-map "app/dev/js/test.js.map"
+                           :pretty-print true
+                           :output-wrapper true}}
+    :prod-main {:source-paths ["src"]
+                :incremental true
+                :jar true
+                :assert true
+                :compiler {:output-to "app/prod/js/cljsbuild-main.js"
+                           :externs ["app/prod/js/externs.js"
+                                     "node_modules/closurecompiler-externs/path.js"
+                                     "node_modules/closurecompiler-externs/process.js"]
+                           :warnings true
+                           :elide-asserts true
+                           :target :nodejs
 
-                                              ;; no optimize compile (dev)
-                                              ;;:optimizations :none
-                                              :output-dir "app/prod/js/out_main"
+                           ;; no optimize compile (dev)
+                           ;;:optimizations :none
+                           :output-dir "app/prod/js/out_main"
 
-                                              ;; simple compile (dev)
-                                              :optimizations :simple
+                           ;; simple compile (dev)
+                           :optimizations :simple
 
-                                              ;; advanced compile (prod)
-                                              ;;:optimizations :advanced
+                           ;; advanced compile (prod)
+                           ;;:optimizations :advanced
 
-                                              ;;:source-map "app/prod/js/test.js.map"
-                                              :pretty-print true
-                                              :output-wrapper true}}
-                       :prod-front {:source-paths ["src_front" "src_front_profile/anh_front/prod"]
-                                    :incremental true
-                                    :jar true
-                                    :assert true
-                                    :compiler {:output-to "app/prod/js/front.js"
-                                               :externs ["app/prod/js/externs_front.js"]
-                                               :warnings true
-                                               :elide-asserts true
-                                               ;; :target :nodejs
+                           ;;:source-map "app/prod/js/test.js.map"
+                           :pretty-print true
+                           :output-wrapper true}}
+    :prod-front {:source-paths ["src_front" "src_front_profile/anh_front/prod"]
+                 :incremental true
+                 :jar true
+                 :assert true
+                 :compiler {:output-to "app/prod/js/front.js"
+                            :externs ["app/prod/js/externs_front.js"]
+                            :warnings true
+                            :elide-asserts true
+                            ;; :target :nodejs
 
-                                               ;; no optimize compile (dev)
-                                               ;;:optimizations :none
-                                               :output-dir "app/prod/js/out_front"
+                            ;; no optimize compile (dev)
+                            ;;:optimizations :none
+                            :output-dir "app/prod/js/out_front"
 
-                                               ;; simple compile (dev)
-                                               :optimizations :simple
+                            ;; simple compile (dev)
+                            :optimizations :simple
 
-                                               ;; advanced compile (prod)
-                                               ;;:optimizations :advanced
+                            ;; advanced compile (prod)
+                            ;;:optimizations :advanced
 
-                                               ;;:source-map "app/prod/js/test.js.map"
-                                               :pretty-print true
-                                               :output-wrapper true}}}}
+                            ;;:source-map "app/prod/js/test.js.map"
+                            :pretty-print true
+                            :output-wrapper true}}}}
   :figwheel {:http-server-root "public"
              :ring-handler figwheel-middleware/app
              :server-port 3441})
