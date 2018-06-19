afa3d083d3119e0c68620602e9f55b3fe4edabc4
iain <iain@soulflyer.co.uk>
2018-06-19 22:50:20 +0700

    keywording buttons added, incl right click events

--- src_front/anh_front/ui_components/keywording.cljs
+++ src_front/anh_front/ui_components/keywording.cljs
@@ -4,8 +4,28 @@
 
 (defn panel
   []
-  (let [keywords (rf/subscribe [:keyword-set])]
+  (let [keywords          (rf/subscribe [:keyword-set])
+        header-background (rf/subscribe [:details-header-background])
+        background        (rf/subscribe [:details-background])]
     [rc/v-box
+     :height "100%"
+     :justify :between
      :children
-     (for [keyword @keywords]
-       [:p keyword])]))
+     [[rc/v-box
+       :style {:margin-top "5px"}
+       :children
+       (for [keyword @keywords]
+         (if keyword ;; TODO keyword-set sometimes has a nil in it. Don't know why.
+           [rc/button
+            :label keyword
+            :style {:padding "0px"
+                    :width "100%"
+                    :border (str "solid 1px " @header-background)
+                    :background @background}
+            :attr {:on-context-menu #(rf/dispatch [:say-hello (str " bye " keyword)])}
+            :on-click #(rf/dispatch [:say-hello keyword])]))]
+      [rc/h-box
+       :children
+       [[rc/button
+         :label "Used Keywords"
+         :on-click #(rf/dispatch [:fill-keyword-set])]]]]]))
