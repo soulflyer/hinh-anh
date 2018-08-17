80db916c343b5a1547e28ae8c51429cb812f98f2
iain <iain@soulflyer.co.uk>
2018-08-17 15:18:14 +0700

    fixed broken refactor

--- src_front/anh_front/events/preference-events.cljs
+++ src_front/anh_front/events/preference-events.cljs
@@ -1,10 +1,5 @@
 (ns anh-front.preference-events
   (:require [anh-front.db  :as db]
-            [re-frame.core :as rf]
-            [akiroz.re-frame.storage :as store]
-            [anh-front.db :as db]
-            [anh-front.tree :as tree]
-            [clojure.set :as set]
             [re-frame.core :as rf]))
 
 (rf/reg-event-db
@@ -17,20 +12,3 @@
   (fn [db _]
     (let [view-fullsize (rf/subscribe [:view-fullsize])]
       (assoc-in db [:preferences :view-fullsize] (not @view-fullsize)))))
-
-(store/reg-co-fx! :anh
-                  {:fx   :store
-                   :cofx :store})
-
-(rf/reg-event-fx
-  :store-preferences
-  [(rf/inject-cofx :store)]
-  (let [prefs (rf/subscribe [:preferences])]
-    (fn [{:keys [store db]} _]
-      {:store (assoc store :preferences @prefs)})))
-
-(rf/reg-event-fx
-  :load-preferences
-  [(rf/inject-cofx :store)]
-  (fn [{:keys [db store]} _]
-    {:db (assoc db :preferences (:preferences store))}))
