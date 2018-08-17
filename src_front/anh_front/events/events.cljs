693355ff1eb764d1292a27f00c16bb0e5146b638
iain <iain@soulflyer.co.uk>
2018-08-17 15:51:48 +0700

    More refactoring of events

--- src_front/anh_front/events/events.cljs
+++ src_front/anh_front/events/events.cljs
@@ -1,9 +1,8 @@
 (ns anh-front.events
   (:require [akiroz.re-frame.storage :as store]
-            [anh-front.db            :as db]
-            [anh-front.tree          :as tree]
-            [clojure.set             :as set]
-            [re-frame.core           :as rf]))
+            [anh-front.db :as db]
+            [clojure.set :as set]
+            [re-frame.core :as rf]))
 
 (def bogus "gotta define something or re-frame-jump-to-reg doesn't find the ns")
 
@@ -148,67 +147,6 @@
       ;;TODO can I call scroll-into-view on :focused-pic-path here?
       {:dispatch-n [[:set-display new-index]
                     [:set-keys :pictures]]})))
-
-(rf/reg-event-fx
-  :fetch-pictures
-  (fn [{:keys [db]} _]
-    (println "Fetch pictures")
-    (let [path (:focus (:project-tree db))]
-      {:dispatch [:load-pictures-for-project path]})))
-
-(rf/reg-event-fx
-  :next-project-open
-  (fn [{:keys [db]} _]
-    (let [tr (:project-tree db)
-          path (:focus tr)
-          newpath (tree/next-node tr path)]
-      {:dispatch-n [(when (= 3 (count newpath))
-                      [:load-pictures-for-project newpath])
-                    [:next-node :project-tree]]
-       :dispatch-later [{:ms 200
-                         :dispatch [:clear-focus]}]})))
-
-(rf/reg-event-fx
-  :prev-project-open
-  (fn [{:keys [db]} _]
-    (let [tr (:project-tree db)
-          path (:focus tr)
-          newpath (tree/prev-node tr path)]
-      {:dispatch-n [(when (= 3 (count newpath))
-                      [:load-pictures-for-project newpath])
-                    [:prev-node :project-tree]]
-       :dispatch-later [{:ms 200
-                         :dispatch [:clear-focus]}]})))
-
-
-(rf/reg-event-fx
-  :go-to-project
-  (fn [{:keys [db]} [_ path]]
-    {:db (-> db
-             (assoc-in [:project-tree :focus] path))
-     :dispatch-n [[:load-pictures-for-project path]
-                  [:expand-path :project-tree]]}))
-
-(rf/reg-event-fx
-  :go-to-saved-project
-  (fn [{:keys [db]} _]
-    (let [saved-project (rf/subscribe [:last-project])]
-      {:dispatch [:go-to-project @saved-project]})))
-
-(rf/reg-event-fx
-  :open-last-visited-project
-  (fn [{:keys [db]} _]
-    ;; TODO this is only delayed because project_tree.cljs is returning the root level and
-    ;; overwriting the :focus. Can be simplified when I get round to fixing that.
-    {:dispatch-later [{:ms 500
-                       :dispatch [:go-to-saved-project]}]}))
-
-(rf/reg-event-fx
-  :open-project-external
-  (fn [{:keys [db]} _]
-    (let [proj (rf/subscribe [:selected-project])
-          path (reduce str (interpose "/" @proj))]
-      {:dispatch [:open-project path]})))
 
 (rf/reg-event-db
   :say-hello
