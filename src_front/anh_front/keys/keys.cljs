d5be5d770ad4e4bc067b8a5c2810bab898e8fe0f
iain <iain@soulflyer.co.uk>
2019-05-28 12:06:50 +0700

    fixed re-pressed 0.3.0 breaking change

--- src_front/anh_front/keys/keys.cljs
+++ src_front/anh_front/keys/keys.cljs
@@ -6,196 +6,196 @@
 
 (def prevent-keys
   [;;{:which (key-codes "return")}
-   {:which (key-codes "up")}
-   {:which (key-codes "down")}
-   {:which (key-codes "space")}
-   {:which (key-codes "a")
+   {:keyCode (key-codes "up")}
+   {:keyCode (key-codes "down")}
+   {:keyCode (key-codes "space")}
+   {:keyCode (key-codes "a")
     :metaKey true}])
 
 (def always-listen-keys
-  [{:which (key-codes "esc")}
-   ;;{:which (key-codes "space")}
-   {:which (key-codes "return")}
+  [{:keyCode (key-codes "esc")}
+   ;;{:keyCode (key-codes "space")}
+   {:keyCode (key-codes "return")}
    ])
 
 (def common-keys
   [[[:inc-picture-columns]
-    [{:which (key-codes "right")
+    [{:keyCode (key-codes "right")
       :ctrlKey true}]]
    [[:dec-picture-columns]
-    [{:which (key-codes "left")
+    [{:keyCode (key-codes "left")
       :ctrlKey true}]]
    [[:set-keys :projects]
-    [{:which (key-codes ",")}]]
+    [{:keyCode (key-codes ",")}]]
    [[:set-keys :pictures]
-    [{:which (key-codes ".")}]]
+    [{:keyCode (key-codes ".")}]]
    [[:clear-all]
-    [{:which (key-codes "delete")}]]
+    [{:keyCode (key-codes "delete")}]]
    [[:select-all]
-    [{:which (key-codes "a")
+    [{:keyCode (key-codes "a")
       :metaKey true}]]
    [[:open-project-external]
-    [{:which (key-codes "p")
+    [{:keyCode (key-codes "p")
       :metaKey true}]]
    [[:open-pictures]
-    [{:which (key-codes "o")
+    [{:keyCode (key-codes "o")
       :metaKey true}]]
    [[:next-panel]
-    [{:which (key-codes "/")}]]
+    [{:keyCode (key-codes "/")}]]
    [[:toggle-footer]
-    [{:which (key-codes "f")
+    [{:keyCode (key-codes "f")
       :metaKey true}]]
    [[:rotate-left-panel]
-    [{:which (key-codes "q")
+    [{:keyCode (key-codes "q")
       :ctrlKey false}]]
    [[:set-left-panel :projects]
-    [{:which (key-codes "w")}]]
+    [{:keyCode (key-codes "w")}]]
    [[:set-left-panel :keywording]
-    [{:which (key-codes "e")
+    [{:keyCode (key-codes "e")
       :ctrlKey false
       :shiftKey false}]]
    [[:set-left-panel :keywords]
-    [{:which (key-codes "r")
+    [{:keyCode (key-codes "r")
       :shiftKey false
       :ctrlKey false}]]
    [[:fill-keyword-set]
-    [{:which (key-codes "u")}]]
+    [{:keyCode (key-codes "u")}]]
    [[:picture-filter-stars 5]
-    [{:which (key-codes "5")
+    [{:keyCode (key-codes "5")
       :metaKey true}]]
    [[:picture-filter-stars 4]
-    [{:which (key-codes "4")
+    [{:keyCode (key-codes "4")
       :metaKey true}]]
    [[:picture-filter-stars 3]
-    [{:which (key-codes "3")
+    [{:keyCode (key-codes "3")
       :metaKey true}]]
    [[:picture-filter-stars 2]
-    [{:which (key-codes "2")
+    [{:keyCode (key-codes "2")
       :metaKey true}]]
    [[:picture-filter-stars 1]
-    [{:which (key-codes "1")
+    [{:keyCode (key-codes "1")
       :metaKey true}]]
    [[:picture-filter-stars 0]
-    [{:which (key-codes "0")
+    [{:keyCode (key-codes "0")
       :metaKey true}]]
    [[:rating 5]
-    [{:which (key-codes "5")
+    [{:keyCode (key-codes "5")
       :metaKey false}]]
    [[:rating 4]
-    [{:which (key-codes "4")
+    [{:keyCode (key-codes "4")
       :metaKey false}]]
    [[:rating 3]
-    [{:which (key-codes "3")
+    [{:keyCode (key-codes "3")
       :metaKey false}]]
    [[:rating 2]
-    [{:which (key-codes "2")
+    [{:keyCode (key-codes "2")
       :metaKey false}]]
    [[:rating 1]
-    [{:which (key-codes "1")
+    [{:keyCode (key-codes "1")
       :metaKey false}]]
    [[:rating 0]
-    [{:which (key-codes "0")
+    [{:keyCode (key-codes "0")
       :metaKey false}]]
    [[:close-popovers]
-    [{:which (key-codes "esc")}]]])
+    [{:keyCode (key-codes "esc")}]]])
 
 (def details-keys
   [[[:say-hello "Details"]
-    [{:which (key-codes "down")}]]])
+    [{:keyCode (key-codes "down")}]]])
 
 (def project-keys
   [[[:next-project-open]
-    [{:which (key-codes "down")
+    [{:keyCode (key-codes "down")
       :ctrlKey false}]]
    [[:prev-project-open]
-    [{:which (key-codes "up")
+    [{:keyCode (key-codes "up")
       :ctrlKey false}]]
    [[:up-or-close :project-tree]
-    [{:which (key-codes "left")}]]
+    [{:keyCode (key-codes "left")}]]
    [[:toggle-button-show atoms/export-json-button-show]
-    [{:which (key-codes "j")}]]
+    [{:keyCode (key-codes "j")}]]
    [[:expand-selected :project-tree]
-    [{:which (key-codes "right")
+    [{:keyCode (key-codes "right")
       :ctrlKey false}]]])
 
 (def keyword-keys
   [[[:next-keyword-open]
-    [{:which (key-codes "down")
+    [{:keyCode (key-codes "down")
       :ctrlKey false}]]
    [[:prev-keyword-open]
-    [{:which (key-codes "up")
+    [{:keyCode (key-codes "up")
       :ctrlKey false}]]
    [[:up-or-close :keyword-tree]
-    [{:which (key-codes "left")}]]
+    [{:keyCode (key-codes "left")}]]
    [[:expand-selected :keyword-tree]
-    [{:which (key-codes "right")}]]
+    [{:keyCode (key-codes "right")}]]
    [[:toggle-keyword-sub-display]
-    [{:which (key-codes "a")}]]
+    [{:keyCode (key-codes "a")}]]
    [[:toggle-keyword-pic-display]
-    [{:which (key-codes "b")}]]
+    [{:keyCode (key-codes "b")}]]
    [[:toggle-button-show atoms/keyword-add-button-show]
-    [{:which (key-codes "=")}]]
+    [{:keyCode (key-codes "=")}]]
    [[:toggle-button-show atoms/keyword-delete-button-show]
-    [{:which (key-codes "-")}]]
+    [{:keyCode (key-codes "-")}]]
    [[:toggle-button-show atoms/keyword-move-button-show]
-    [{:which (key-codes "m")
+    [{:keyCode (key-codes "m")
       :shiftKey false}]]
    [[:toggle-button-show atoms/keyword-merge-button-show]
-    [{:which (key-codes "m")
+    [{:keyCode (key-codes "m")
       :shiftKey true}]]
    [[:toggle-button-show atoms/keyword-rename-button-show]
-    [{:which (key-codes "r")
+    [{:keyCode (key-codes "r")
       :ctrlKey true}]]
    [[:toggle-button-show atoms/keyword-set-best-button-show]
-    [{:which (key-codes "s")}]]
+    [{:keyCode (key-codes "s")}]]
    [[:toggle-button-show atoms/keyword-add-orphans-button-show]
-    [{:which (key-codes "o")}]]
+    [{:keyCode (key-codes "o")}]]
    [[:toggle-button-show atoms/go-to-project-button-show]
-    [{:which (key-codes "p")
+    [{:keyCode (key-codes "p")
       :shiftKey false}]]
    [[:toggle-button-show atoms/keyword-purge-unused-button-show]
-    [{:which (key-codes "p")
+    [{:keyCode (key-codes "p")
       :shiftKey true}]]
    [[:load-keyword-tree]
-    [{:which (key-codes "r")
+    [{:keyCode (key-codes "r")
       :shiftKey true}]]
    [[:toggle-button-show atoms/keyword-find-button-show]
-    [{:which (key-codes "f")}]]
+    [{:keyCode (key-codes "f")}]]
    [[:toggle-button-show atoms/export-json-button-show]
-    [{:which (key-codes "j")}]]
+    [{:keyCode (key-codes "j")}]]
    ;; [[:close-popovers]
-   ;;  [{:which (key-codes "return")}]]
+   ;;  [{:keyCode (key-codes "return")}]]
    ])
 
 (def picture-keys
   [[[:next-picture]
-    [{:which (key-codes "right")
+    [{:keyCode (key-codes "right")
       :ctrlKey false}]]
    [[:prev-picture]
-    [{:which (key-codes "left")
+    [{:keyCode (key-codes "left")
       :ctrlKey false}]]
    [[:down-picture]
-    [{:which (key-codes "down")
+    [{:keyCode (key-codes "down")
       :ctrlKey false}]]
    [[:up-picture]
-    [{:which (key-codes "up")
+    [{:keyCode (key-codes "up")
       :ctrlKey false}]]
    [[:toggle-select-focused-pic]
-    [{:which (key-codes "space")}]]
+    [{:keyCode (key-codes "space")}]]
    [[:rotate-display]
-    [{:which (key-codes "return")}]]])
+    [{:keyCode (key-codes "return")}]]])
 
 (def keywording-keys
   [[[:toggle-edit-keywords]
-    [{:which (key-codes "e")
+    [{:keyCode (key-codes "e")
       :ctrlKey true}]]
    [[:toggle-edit-keyword-sets]
-    [{:which (key-codes "e")
+    [{:keyCode (key-codes "e")
       :shiftKey true}]]
    [[:close-popovers]
-    [{:which (key-codes "return")}]]
+    [{:keyCode (key-codes "return")}]]
    [[:toggle-button-show atoms/save-keyword-set-show]
-    [{:which (key-codes "s")
+    [{:keyCode (key-codes "s")
       :ctrlKey true}]]])
 
 (def project-key-set
@@ -216,7 +216,7 @@
 (def details-key-set
   {:event-keys (into common-keys picture-keys)
    :prevent-default-keys prevent-keys
-   :always-listen-keys [{:which 13}]})
+   :always-listen-keys [{:keyCode 13}]})
 
 (defn keywording-key-set []
   {:event-keys (reduce into
